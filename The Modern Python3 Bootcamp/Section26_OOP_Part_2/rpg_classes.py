c360dd28284b246c74a8b587dbcb5aa247c744d6
unknown <jgabunil@ucla.edu>
2021-02-11 19:34:20 -0800

    Updating progress

---
+++ The Modern Python3 Bootcamp/Section26_OOP_Part_2/rpg_classes.py
@@ -0,0 +1,22 @@
+# Role-playing game classes exercise
+
+class Character:
+        def __init__(self, name, hp, level):
+                self.name = name
+                self.hp = hp
+                self.level = level
+
+# NPC subclass (child class)
+class NPC(Character):
+        def __init__(self, name, hp, level):
+                # Initialize based on the parent character class
+                super().__init__(name, hp, level)
+        
+        def speak(self):
+                return 'I used to be an adventurer like you, but then I took an arrow to the knee.'
+
+villager = NPC("Bob", 100, 12)
+print(villager.name)
+print(villager.hp)
+print(villager.level)
+print(villager.speak())
